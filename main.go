821e1c09d5e46f877bee0c6ceb44a52cfd4761b6
Alex Turner <alex.turner@safetyculture.io>
2024-01-29 22:52:54 +1100

    mashed together an htmx demo

--- main.go
+++ main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
+
+	"github.com/a-h/templ"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
-}
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
-}
+// func getRoot(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Printf("got / request\n")
+// 	io.WriteString(w, "This is my website!\n")
+// }
+
+// func getHello(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Printf("got /hello request\n")
+// 	io.WriteString(w, "Hello, HTTP!\n")
+// }
 
 func main() {
+	page := Page()
+	hello := hello("STEEEEEVE")
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", getRoot)
-	mux.HandleFunc("/hello", getHello)
+	mux.Handle("/", templ.Handler(page))
+	// mux.HandleFunc("/hello", templ.Handler(component))
+	mux.Handle("/hello", templ.Handler(hello))
 
 	err := http.ListenAndServe(":3333", mux)
 	if errors.Is(err, http.ErrServerClosed) {
